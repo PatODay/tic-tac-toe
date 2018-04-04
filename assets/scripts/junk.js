555e35194cb9db76fc8c7b7ff0166722a360df4f
PatODay <poday22@gmail.com>
2018-04-04 12:24:40 -0400

    Added auth folder for API.js
    Began working on authentication API

--- assets/scripts/junk.js
+++ assets/scripts/junk.js
@@ -31,7 +31,7 @@
 //
 // const win = () => {
 //   if (winCombo.some((key) => {
-    // return key.join('') === 'XXX'
+// return key.join('') === ''
 //   })) {
 //     playerXWon()
 //   } else if (winCombo.some((key) => {
@@ -87,77 +87,76 @@
 //   }
 // }
 
+// if (board[0].innerHTML === 'X' && board[0].innerHTML === board[1].innerHTML && board[1].innerHTML === board[2].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[3].innerHTML === 'X' && board[3].innerHTML === board[4].innerHTML && board[3].innerHTML === board[5].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[6].innerHTML === 'X' && board[6].innerHTML === board[7].innerHTML && board[6].innerHTML === board[8].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[0].innerHTML === 'X' && board[0].innerHTML === board[3].innerHTML && board[0].innerHTML === board[6].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[1].innerHTML === 'X' && board[1].innerHTML === board[4].innerHTML && board[1].innerHTML === board[7].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[2].innerHTML === 'X' && board[2].innerHTML === board[5].innerHTML && board[2].innerHTML === board[8].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[0].innerHTML === 'X' && board[0].innerHTML === board[4].innerHTML && board[0].innerHTML === board[8].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[2].innerHTML === 'X' && board[2].innerHTML === board[4].innerHTML && board[2].innerHTML === board[6].innerHTML) {
+//   console.log('X wins!')
+// } else if (board[0].innerHTML === 'O' && board[0].innerHTML === board[1].innerHTML && board[1].innerHTML === board[2].innerHTML) {
+//   console.log('O wins!')
+// } else if (board[3].innerHTML === 'O' && board[3].innerHTML === board[4].innerHTML && board[3].innerHTML === board[5].innerHTML) {
+//   console.log('O wins!')
+// } else if (board[6].innerHTML === 'O' && board[6].innerHTML === board[7].innerHTML && board[6].innerHTML === board[8].innerHTML) {
+//   console.log('O wins!')
+// } else if (board[0].innerHTML === 'O' && board[0].innerHTML === board[3].innerHTML && board[0].innerHTML === board[6].innerHTML) {
+//   console.log('O wins!')
+// } else if (board[1].innerHTML === 'O' && board[1].innerHTML === board[4].innerHTML && board[1].innerHTML === board[7].innerHTML) {
+//   console.log('O wins!')
+// } else if (board[2].innerHTML === 'O' && board[2].innerHTML === board[5].innerHTML && board[2].innerHTML === board[8].innerHTML) {
+//   console.log('O wins!')
+// } else if (board[0].innerHTML === 'O' && board[0].innerHTML === board[4].innerHTML && board[0].innerHTML === board[8].innerHTML) {
+//   console.log('O wins!')
+// } else if (board[2].innerHTML === 'O' && board[2].innerHTML === board[4].innerHTML && board[2].innerHTML === board[6].innerHTML) {
+//   console.log('O wins!')
+// }
+// }
 
-  // if (board[0].innerHTML === 'X' && board[0].innerHTML === board[1].innerHTML && board[1].innerHTML === board[2].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[3].innerHTML === 'X' && board[3].innerHTML === board[4].innerHTML && board[3].innerHTML === board[5].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[6].innerHTML === 'X' && board[6].innerHTML === board[7].innerHTML && board[6].innerHTML === board[8].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[0].innerHTML === 'X' && board[0].innerHTML === board[3].innerHTML && board[0].innerHTML === board[6].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[1].innerHTML === 'X' && board[1].innerHTML === board[4].innerHTML && board[1].innerHTML === board[7].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[2].innerHTML === 'X' && board[2].innerHTML === board[5].innerHTML && board[2].innerHTML === board[8].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[0].innerHTML === 'X' && board[0].innerHTML === board[4].innerHTML && board[0].innerHTML === board[8].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[2].innerHTML === 'X' && board[2].innerHTML === board[4].innerHTML && board[2].innerHTML === board[6].innerHTML) {
-  //   console.log('X wins!')
-  // } else if (board[0].innerHTML === 'O' && board[0].innerHTML === board[1].innerHTML && board[1].innerHTML === board[2].innerHTML) {
-  //   console.log('O wins!')
-  // } else if (board[3].innerHTML === 'O' && board[3].innerHTML === board[4].innerHTML && board[3].innerHTML === board[5].innerHTML) {
-  //   console.log('O wins!')
-  // } else if (board[6].innerHTML === 'O' && board[6].innerHTML === board[7].innerHTML && board[6].innerHTML === board[8].innerHTML) {
-  //   console.log('O wins!')
-  // } else if (board[0].innerHTML === 'O' && board[0].innerHTML === board[3].innerHTML && board[0].innerHTML === board[6].innerHTML) {
-  //   console.log('O wins!')
-  // } else if (board[1].innerHTML === 'O' && board[1].innerHTML === board[4].innerHTML && board[1].innerHTML === board[7].innerHTML) {
-  //   console.log('O wins!')
-  // } else if (board[2].innerHTML === 'O' && board[2].innerHTML === board[5].innerHTML && board[2].innerHTML === board[8].innerHTML) {
-  //   console.log('O wins!')
-  // } else if (board[0].innerHTML === 'O' && board[0].innerHTML === board[4].innerHTML && board[0].innerHTML === board[8].innerHTML) {
-  //   console.log('O wins!')
-  // } else if (board[2].innerHTML === 'O' && board[2].innerHTML === board[4].innerHTML && board[2].innerHTML === board[6].innerHTML) {
-  //   console.log('O wins!')
-  // }
-  // }
+// const events = require('./events')
+// Assigning 'x' and 'o' to players
+// const player1 = 'X'
+// const player2 = 'O'
+// const players = ['X', 'O']
+// const markers = ['X', 'O']
 
-  // const events = require('./events')
-  // Assigning 'x' and 'o' to players
-  // const player1 = 'X'
-  // const player2 = 'O'
-  // const players = ['X', 'O']
-  // const markers = ['X', 'O']
+// const changeTurn = function () {
+//   if (turn === player1) {
+//     turn = player2
+//   } else (turn = player1)
+//   console.log(turn)
+// }
 
-  // const changeTurn = function () {
-  //   if (turn === player1) {
-  //     turn = player2
-  //   } else (turn = player1)
-  //   console.log(turn)
-  // }
+// const play = function (div) {
+//   return changeTurn
+// }
+// check open tiles on game board
+// if active player clicks on unoccupied tile place player icon there
+// reset game
+// show user games played
 
-  // const play = function (div) {
-  //   return changeTurn
-  // }
-  // check open tiles on game board
-  // if active player clicks on unoccupied tile place player icon there
-  // reset game
-  // show user games played
+// Winning combinations based on array index
+// const wins = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ]
 
-  // Winning combinations based on array index
-  // const wins = [
-  //   [0, 1, 2],
-  //   [3, 4, 5],
-  //   [6, 7, 8],
-  //   [0, 3, 6],
-  //   [1, 4, 7],
-  //   [2, 5, 8],
-  //   [0, 4, 8],
-  //   [2, 4, 6]
-  // ]
-
-  module.exports = {
-    // player1,
-    // player2
-  }
+module.exports = {
+  // player1,
+  // player2
+}
