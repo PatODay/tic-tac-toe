487a686abd6fbec198dda0807390bad7843c0dde
PatODay <poday22@gmail.com>
2018-04-04 11:27:05 -0400

    Junk file
    Added junk file for old code

--- assets/scripts/junk.js
+++ assets/scripts/junk.js
@@ -122,3 +122,42 @@
   //   console.log('O wins!')
   // }
   // }
+
+  // const events = require('./events')
+  // Assigning 'x' and 'o' to players
+  // const player1 = 'X'
+  // const player2 = 'O'
+  // const players = ['X', 'O']
+  // const markers = ['X', 'O']
+
+  // const changeTurn = function () {
+  //   if (turn === player1) {
+  //     turn = player2
+  //   } else (turn = player1)
+  //   console.log(turn)
+  // }
+
+  // const play = function (div) {
+  //   return changeTurn
+  // }
+  // check open tiles on game board
+  // if active player clicks on unoccupied tile place player icon there
+  // reset game
+  // show user games played
+
+  // Winning combinations based on array index
+  // const wins = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6]
+  // ]
+
+  module.exports = {
+    // player1,
+    // player2
+  }
